2c3ac368d92106a92240ace31c435b2155ad5053
Benoit Hirbec <bhirbec@twitter.com>
2018-03-19 22:30:11 -0700

    Producer can insert more tasks

--- producer.py
+++ producer.py
@@ -11,20 +11,18 @@ parser.add_option("-n", "--number-task", type=int, dest="number_tasks", default=
 def main(options):
   engine = create_engine('mysql://ndp:password@localhost:3306/ndp')
 
-  sql = '''
-    insert into task
-        (host, commands)
-    values
-        ('smf1-cs{0}-12', 'show hardware'),
-        ('smf1-cs{0}-13', 'show hardware'),
-        ('smf1-cs{0}-14', 'show hardware'),
-        ('smf1-cs{0}-15', 'show hardware')
-  '''
+  values = ',\n'.join(
+    "('smf1-cs-{0}', 'show hardware')".format(i)
+    for i in range(300)
+  )
+
+  sql = 'insert into task(host, commands) values {0}'.format(values)
+
   for i in range(options.number_tasks):
     with engine.begin() as con:
       print 'Inserting tasks...'
       con.execute(sql.format(i))
-      time.sleep(0.01)
+      time.sleep(1)
 
 
 if __name__ == '__main__':
